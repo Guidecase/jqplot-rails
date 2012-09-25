b123989a59d5f44a468a6e1e6e8fd215d99c5221
Travis Dunn <dunn.travis@gmail.com>
2012-09-25 12:56:51 +0200

    catch internal errors and display feedback in chart element

--- lib/jqplot-rails/action_view/helpers/jqplot_helper.rb
+++ lib/jqplot-rails/action_view/helpers/jqplot_helper.rb
@@ -5,7 +5,8 @@ module ActionView
 
       def jqplot(height, width, data_series, options={})
       	dom_id = SecureRandom.hex(12)
-        tag "div", :data => {:series => data_series, :options => options, :role => 'jqplot'}
+        tag "div", :data => {:series => data_series, :options => options, :role => 'jqplot'}, 
+        	       :id => dom_id
       end
   	end
   end
