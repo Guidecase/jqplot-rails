7599e09727c1b29c755fc0b63af5f90ffbfbe328
Travis Dunn <dunn.travis@gmail.com>
2012-09-28 16:20:44 +0200

    size plot in given units or default pixels

--- lib/jqplot-rails/action_view/helpers/jqplot_helper.rb
+++ lib/jqplot-rails/action_view/helpers/jqplot_helper.rb
@@ -5,8 +5,10 @@ module ActionView
 
       def jqplot(height, width, data_series, options={})
       	dom_id = SecureRandom.hex(12)
+        height = height.is_a?(String) ? height : "#{height}px"
+        width = width.is_a?(String) ? width : "#{width}px"
         tag "div", :data => {:series => data_series, :options => options, :role => 'jqplot'}, 
-        	         :id => dom_id, :style => "height:#{height}px;width:#{width}px"
+        	         :id => dom_id, :style => "height:#{height};width:#{width}"
       end
   	end
   end
